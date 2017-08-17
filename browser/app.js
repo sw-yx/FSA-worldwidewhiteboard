aaf226dcdaf5c71a6240579ca4327caf31451a06
sw-yx <swyx@swyx.io>
2017-08-17 16:22:40 -0400

    refactor

--- browser/app.js
+++ browser/app.js
@@ -1,14 +1,10 @@
-var socket = io(window.location.origin);
+const socket = io(window.location.origin);
+const drawdata = (data) => window.whiteboard.draw(data.start, data.end, data.strokeColor)
 
 socket.on("drew", drawdata)
 
 socket.on('connect', () => console.log('I have made a persistent two-way connection to the server!'));
 
-function drawdata(data) {
-    window.whiteboard.draw(data.start, data.end, data.strokeColor)
-}
-socket.on('firstConnect', state => {
-    state.forEach(drawdata);
-})
+socket.on('firstConnect', state => state.forEach(drawdata))
 
 window.whiteboard.on("draw", (start, end, strokeColor) => socket.emit("drewsomething", {start, end, strokeColor}))
